c2d3badb0af473d834258b03d98cb1d7fc57e184
Gianni Zamora <giannizcool@gmail.com>
2021-12-03 11:34:32 -0700

    init commit

---
+++ app/Http/Controllers/TimeslotController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTimeslotRequest;
+use App\Http\Requests\UpdateTimeslotRequest;
+use App\Models\Timeslot;
+
+class TimeslotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTimeslotRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTimeslotRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Timeslot  $timeslot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Timeslot $timeslot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Timeslot  $timeslot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Timeslot $timeslot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTimeslotRequest  $request
+     * @param  \App\Models\Timeslot  $timeslot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTimeslotRequest $request, Timeslot $timeslot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Timeslot  $timeslot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Timeslot $timeslot)
+    {
+        //
+    }
+}
